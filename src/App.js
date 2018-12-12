ad5ce5480a8855feafa432ecaa36f2b9ce8b2424
Ruslana Solovova <ruslanasolovova@gmail.com>
2018-12-12 14:40:11 -0800

    map inint

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount(){    //render map on page
+    this.loadMap()
+  }
+
+  loadMap = () => {
+    this.mapScript("https://maps.googleapis.com/maps/api/js?  key=AIzaSyAkOOpjPuHxiUG6e3DhJDIMDYZ5zL9Pr98&callback=initMap")    // Inserted API Key in the below call to load the API
+    window.initMap= this.initMap   // Asynchronously load the Google Maps script, passing in the callback reference
+  }
+
+  initMap = () => {   //function to initialize the map
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+  }
+
+  mapScript = (url) => {       //this function is outside the react component
+    let index = window.document.getElementsByTagName("script")[0] //select script tag
+    let script = window.document.createElement("script")    //created script tag
+    script.src = url
+    script.async = true     // Asynchronously load the Google Maps script
+    script.defer = true 
+    index.parentNode.insertBefore(script, index)
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <div id="map">
+      
+        </div>
       </div>
     );
   }
 }
+
+
+  // <script async defer 
+  // src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAkOOpjPuHxiUG6e3DhJDIMDYZ5zL9Pr98&callback=initMap">
+  // </script>  
+ // Inserted API Key in the below call to load the API
+
+// function mapScript(url){       //this function is outside the react component
+//   let index = window.document.getElementsByTagName("script")[0] //select script tag
+//   let script = window.document.createElement("script")    //created script tag
+//   script.src = url
+//   script.async = true     //Load the JS API ASYNCHRONOUSLY
+//   script.defer = true 
+//   index.parentNode.insertBefore(script, index)
+// }
+
 
 export default App;
